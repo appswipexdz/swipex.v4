d9a98e553b7a19cf8c4f3465bd511f7a1f6ad843
appswipexdz <app.swipex.dz@gmail.com>
2026-09-18 15:19:24 +0100

    مهمة: حذف آمن للملفات التجريبية غير المستخدمة في المشروع

--- extract.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const l = fs.readFileSync('index.html', 'utf8').split('\n');
-const ranges = [[470, 490], [2625, 2665], [1800, 1880], [3929, 4000], [4930, 5000]];
-ranges.forEach(([a, b]) => {
-  console.log('===== LINES ' + a + '-' + b + ' =====');
-  for (let i = a; i <= b && i <= l.length; i++) console.log(i + ': ' + l[i - 1]);
-});
--- extract3.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const l = fs.readFileSync('index.html', 'utf8').split('\n');
-console.log('===== LINES 3095-3140 =====');
-for (let i = 3095; i <= 3140 && i <= l.length; i++) console.log(i + ': ' + l[i - 1]);
